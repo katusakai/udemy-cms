16bf95d3f296dbc983b0a0dd107438e81557646f
Tadas Janca <katusakai@gmail.com>
2019-01-13 11:55:48 +0200

    Made first includes on our template. All programing will start now

---
+++ includes/db.php
@@ -0,0 +1,18 @@
+<?php
+
+$db['DB_HOST'] = "localhost";
+$db['DB_USER'] = "root";
+$db['DB_PASS'] = "";
+$db['DB_NAME'] = "cms";
+
+foreach ($db as $key => $value) {
+  define($key, $value);
+}
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if($connection) {
+  echo "We are connected";
+}
+
+ ?>
